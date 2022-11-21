58eb6f3032b46fb239a79aff8fea81a330bb4a90
HOANG <hoangtvph20603@.fpt.edu.vn>
2022-11-21 23:23:25 +0700

    capNhat GD

--- app/src/main/java/com/fpoly/quanly/QuanLySanPham.java
+++ app/src/main/java/com/fpoly/quanly/QuanLySanPham.java
@@ -5,22 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class QuanLySanPham extends AppCompatActivity {
-    Button btnThemSP;
+    GridView lvSanPham;
+//    Button btnThemSP;
+FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quan_ly_san_pham);
-        btnThemSP = findViewById(R.id.btnThemSanPham);
+        lvSanPham = findViewById(R.id.lvSanPham);
+        fab = findViewById(R.id.fab);
 
-        btnThemSP.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(QuanLySanPham.this,ThemSanPham.class);
                 startActivity(intent);
             }
         });
+//        lvSanPham(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                item = list.get(position);
+//                openDialog(getActivity(),1);//update
+//                return false;
+//            }
+//        });
     }
 }
